d16f3ac57bf643415efad8cb943b4673b7b7d131
Minh Nguyen <mnguyen4725@gmail.com>
2022-09-03 21:01:06 -0700

    Added error message when command line is omitted

--- displayTheLastLines.py
+++ displayTheLastLines.py
@@ -2,22 +2,26 @@ import sys
 import os
 
 
-print(sys.argv)
-if(str(sys.argv[2]).isdigit()):
-    try:
-        with open(sys.argv[1],"r+") as file:
-            try:
-    
-            
-                last_lines = file.readlines()
-                for x in range(-int(sys.argv[2]),0):
-                    print(last_lines[x])
- 
-            
-            except Exception:
-                print("Invalid second argument. File contains lines less than " + str(sys.argv[2]))
+try:
+    if(str(sys.argv[2]).isdigit()):
+        try:
+            with open(sys.argv[1],"r+") as file:
+                try:
         
-    except IOError:
-        print("Invalid first argument. Could not find file " + str(sys.argv[1]))
-else:
-    print("Invalid second argument. Argument is not a number.")
+                
+                    last_lines = file.readlines()
+                    for x in range(-int(sys.argv[2]),0):
+                        print(last_lines[x])
+    
+                
+                except Exception:
+                    print("Invalid second argument. File contains lines less than " + str(sys.argv[2]))
+            
+        except IOError:
+            print("Invalid first argument. Could not find file " + str(sys.argv[1]))
+    else:
+        print("Invalid second argument. Argument is not a number.")
+
+except Exception:
+    print("Command Line argument ommitted.")
+    
